d2963389e501248a9bc1bebee76320daae4e8f16
Sonhong1805 <nguyenhongson18052003@gmail.com>
2024-11-20 22:27:11 +0700

    check image empty

--- index.js
+++ index.js
@@ -17,15 +17,22 @@ app.post(
     { name: "images", maxCount: 10 },
   ]),
   (req, res) => {
-    const { image } = req.files;
-    console.log("image:" + image[0]?.path);
-    // console.log("images:" + req.files.images?.map((file) => file.path));
-
-    res.status(200).json({
-      success: true,
-      message: "upload image successfully",
-      image: image[0]?.path,
-    });
+    if (req.files && req.files.image && req.files.image.length > 0) {
+      const imagePath = req.files.image[0]?.path;
+      if (imagePath) {
+        res.status(200).json({
+          success: true,
+          message: "Upload image successfully",
+          image: imagePath,
+        });
+      }
+    } else {
+      res.status(200).json({
+        success: true,
+        message: "No image uploaded",
+        image: "",
+      });
+    }
   }
 );
 
